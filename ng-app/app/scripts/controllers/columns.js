3ede414ff2477397c0dc9c3d8f275cfefaed5cb2
Kenneth Lim <kennethlimjf@gmail.com>
2014-09-24 05:21:17 +0800

    Some minor updates

--- ng-app/app/scripts/controllers/columns.js
+++ ng-app/app/scripts/controllers/columns.js
@@ -9,11 +9,11 @@ angular.module('skillsortApp.controllers.columns', ['skillsortApp.services.axes'
     $scope.column_4 = [];
     $scope.column_5 = [];
 
+    $scope.axes = Axes.query();
+
     $scope.$on('insertCardEvent', function(event, src){
       var column_number = src[0];
       var card = src[1];
       eval("$scope.column_" + column_number + ".push(card)");
     });
-
-    $scope.axes = Axes.query();
   });
